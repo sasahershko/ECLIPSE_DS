46904b35926986f4cb68954ca292728e8f4c5409
sasahershko <sarayhershko@gmail.com>
2024-02-06 16:08:26 +0100

    si

---
+++ DiseñoSoftware/src/EjerciciosSesion2/Engine.java
@@ -0,0 +1,32 @@
+package EjerciciosSesion2;
+
+public class Engine {
+    public final static String TIPO_POR_DEFECTO = "Eléctrico";
+
+	private String tipo;
+	
+    //DELEGACIÓN DE CONSTRUCTORES
+    public Engine(){
+        this(Engine.TIPO_POR_DEFECTO);
+    }
+	public Engine(String tipo) {
+		this.tipo = tipo;
+	}
+	
+    //MÉTODO
+	public void encender() {
+		System.out.println("¡" + this.tipo + " encendido!");
+	}
+	
+    public String toString(){
+        return "[Tipo de motor: " + this.tipo + "]";  
+    }
+
+	public static void main(String[] args) {
+        Engine engine = new Engine("Eléctrico");
+
+        engine.encender();
+        System.out.println(engine);
+    }
+}
+
